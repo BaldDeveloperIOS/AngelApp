9764eb7eeacb662a17ca9afe5fb74c491cc34079
BaldDeveloperIOS <thebalddeveloper@gmail.com>
2020-05-26 10:28:53 +0200

    First commit.

---
+++ AngelApp/AngelApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AngelApp
+//
+//  Created by chauveau on 26/05/2020.
+//  Copyright © 2020 chauveau. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
